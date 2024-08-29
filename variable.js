aa26c894382c5a0921039196274c3390a24267ce
samadhanturakane06 <75110002+samadhanturakane06@users.noreply.github.com>
2024-08-29 09:21:15 +0000

    basic varibale datatype in javascript

---
+++ variable.js
@@ -0,0 +1,9 @@
+console.log("hello sam");
+let firstName="samadhan";
+var lastName="Turakane";
+console.log(firstName);
+console.log(lastName);
+
+/*don't use var to declare variable becoz var has scope problem..
+it will change same varibale in every file when changes happen in same name variable
+*/
