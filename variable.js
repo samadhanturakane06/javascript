be85d37d77ef0a734293e47794265ffeeee11901
samadhanturakane06 <75110002+samadhanturakane06@users.noreply.github.com>
2024-08-30 09:45:46 +0000

    javascript variable

--- variable.js
+++ variable.js
@@ -1,9 +1,14 @@
 console.log("hello sam");
 let firstName="samadhan";
 var lastName="Turakane";
+const country="India";
+
+//country="Bharat"; //you cant change constant variable in js
+firstName="prashant";
 console.log(firstName);
 console.log(lastName);
+console.log(country);
 
 /*don't use var to declare variable becoz var has scope problem..
-it will change same varibale in every file when changes happen in same name variable
+it will change same varibale in every file when changes happen in same variable name
 */
